79f9b3b347e3c1f1a570cdb761b263a04a97535d
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-10-22 12:05:33 +0530

    Data Created

---
+++ src/Components/FooterLinksData.js
@@ -0,0 +1,124 @@
+const FooteeData = [
+  {
+    headerName : "Orders and support",
+    arrayData : [
+      {
+        name : "Contact us",
+        link : "",
+        icon : true
+      },
+      {
+        name : "FAQs",
+        link : "",
+        icon : true
+      },
+      {
+        name : "Shipping",
+        link : "",
+        icon : true
+      },
+      {
+        name : "Returns",
+        link : "",
+        icon : true
+      },
+      {
+        name : "Terms and Condition",
+        link : "",
+        icon : false
+      }
+    ]
+  },
+  {
+    headerName : "Services",
+    arrayData : [
+      {
+        name : "Live assistance",
+        link : "",
+        icon : false
+      },
+      {
+        name : "Corporate gifts",
+        link : "",
+        icon : false
+      },
+      {
+        name : "Facial appointments",
+        link : "",
+        icon : false
+      },
+    ]
+  },
+  {
+    headerName : "Location preferences",
+    arrayData : [
+      {
+        name : "Shipping: Hong Kong (S.A.R)\nLanguage:English",
+        link : "",
+        icon : false
+      }
+    ]
+  },
+  {
+    headerName : "About",
+    arrayData : [
+      {
+        name : "Our Story",
+        link : "",
+        icon : false
+      },
+      {
+        name : "Foundation",
+        link : "",
+        icon : true
+      },
+      {
+        name : "Careers",
+        link : "",
+        icon : false
+      },
+      {
+        name : "Privacy policy",
+        link : "",
+        icon : false
+      },
+      {
+        name : "Accessibility",
+        link : "",
+        icon : false
+      },
+    ]
+  },
+  {
+    headerName : "Social media",
+    arrayData : [
+      {
+        name : "Instagram",
+        link : "",
+        icon : true
+      },
+      {
+        name : "Twitter",
+        link : "",
+        icon : true
+      },
+      {
+        name : "LinkedIn",
+        link : "",
+        icon : true
+      },
+      {
+        name : "WeChat",
+        link : "",
+        icon : false
+      },
+      {
+        name : "Weibo",
+        link : "",
+        icon : true
+      },
+    ]
+  },
+]
+
+export default FooteeData;
